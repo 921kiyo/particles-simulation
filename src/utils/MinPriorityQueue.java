efa010d1750de49baffd10b89943dccdb4dde4e7
Kiyohito Kunii <kk3317@tui.doc.ic.ac.uk>
2018-02-14 16:56:01 +0000

    add comment on queue

--- src/utils/MinPriorityQueue.java
+++ src/utils/MinPriorityQueue.java
@@ -1,12 +1,22 @@
 package utils;
+import java.util.*;
 
 public class MinPriorityQueue<T extends Comparable<T>> {
-    
+    // has to run with O(log2N)
     /**
+
+    // Linked List structure (singly or doubly linked link)
+
      * Creates an empty queue.
      */
+     int pointer;
+     ArrayList heap;
+     int size;
     public MinPriorityQueue() {
-        // TODO implement the constructor
+       pointer = 0; // Keeps track of the next available index
+       heap = new ArrayList();
+       
+        // Compare between parent and child node, if the child one is greater, switch the two keys
     }
 
     /**
@@ -16,19 +26,25 @@ public class MinPriorityQueue<T extends Comparable<T>> {
         // TODO implement this method
         return 0;
     }
-    
     /**
      * Adds elem to the queue.
      */
     public void add(T elem) {
         // TODO implement this method
+        // increment the pointer
     }
 
     /**
      * Removes, and returns, the element at the front of the queue.
      */
     public T remove() {
+      // returns the smallest element, and remove it from the queue
         // TODO implement this method
+        // return the smallest element
+        // Remove it, take the bottom right node and put it at the top
+        // After that, compare the two children nodes,
+
+        // Decrement the pointer
         return null;
     }
 
@@ -39,5 +55,5 @@ public class MinPriorityQueue<T extends Comparable<T>> {
         // TODO implement this method
         return false;
     }
-    
+
 }
